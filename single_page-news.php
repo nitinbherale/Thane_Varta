f01f84a549faea1e94f3f598f23c227d7843721f
nitinbherale <nitinbherale@gmail.com>
2018-07-06 17:20:27 +0530

    file ADDED

--- single_page-news.php
+++ single_page-news.php
@@ -53,8 +53,132 @@
      <?php include("footer.php") ?>
 
 
-
-
+      <!-- Offcanvas Menu Start -->
+            <div id="offcanvas-body-wrapper" class="offcanvas-body-wrapper">
+                <div id="offcanvas-nav-close" class="offcanvas-nav-close offcanvas-menu-btn">
+                    <a href="#" class="menu-times re-point">
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+                <div class="offcanvas-main-body">
+                    <ul id="accordion" class="offcanvas-nav panel-group">
+                        <li class="panel panel-default">
+                            <div class="panel-heading">
+                                <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                    <i class="fa fa-home" aria-hidden="true"></i>Home Pages</a>
+                            </div>
+                            <div aria-expanded="false" id="collapseOne" role="tabpanel" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul class="offcanvas-sub-nav">
+                                        <li>
+                                            <a href="#">Home 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 4</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 5</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 6</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Home 7</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-user" aria-hidden="true"></i>Author Post Page</a>
+                        </li>
+                        <li class="panel panel-default">
+                            <div class="panel-heading">
+                                <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                                    <i class="fa fa-file-text" aria-hidden="true"></i>Post Pages</a>
+                            </div>
+                            <div aria-expanded="false" id="collapseTwo" role="tabpanel" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul class="offcanvas-sub-nav">
+                                        <li>
+                                            <a href="#">Post Style 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Post Style 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Post Style 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Post Style 4</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="panel panel-default">
+                            <div class="panel-heading">
+                                <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                                    <i class="fa fa-info-circle" aria-hidden="true"></i>News Details Pages</a>
+                            </div>
+                            <div aria-expanded="false" id="collapseThree" role="tabpanel" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul class="offcanvas-sub-nav">
+                                        <li>
+                                            <a href="#">News Details 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">News Details 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">News Details 3</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="archive.html">
+                                <i class="fa fa-archive" aria-hidden="true"></i>Archive Page</a>
+                        </li>
+                        <li class="panel panel-default">
+                            <div class="panel-heading">
+                                <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                                    <i class="fa fa-picture-o" aria-hidden="true"></i>Gallery Pages</a>
+                            </div>
+                            <div aria-expanded="false" id="collapseFour" role="tabpanel" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul class="offcanvas-sub-nav">
+                                        <li>
+                                            <a href="#">Gallery Style 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Gallery Style 2</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>404 Error Page</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-phone" aria-hidden="true"></i>Contact Page</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Offcanvas Menu End -->
 
     </div><!--Wrapper End Here-->
 
